4b71180d021a1dd3a3dca840437fa1cbf83c81a7
Gary Pamparà <gpampara@gmail.com>
2018-02-27 06:38:06 +0200

    Add linking to Cocoa framework on MacOS

--- shell.nix
+++ shell.nix
@@ -4,5 +4,6 @@ stdenv.mkDerivation {
   name = "blog-env";
   buildInputs = [
     (pkgs.haskellPackages.ghcWithPackages (p: with p; [ hakyll ]))
+    pkgs.darwin.apple_sdk.frameworks.Cocoa
   ];
 }
